e2770fe88e49d0c4a34b3c3edffee0005c0330d8
EC2 Default User <ec2-user@ip-172-31-10-249.ap-northeast-1.compute.internal>
2024-07-14 10:25:16 +0000

    Add username field to admin account creation in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 # 管理者アカウントの作成
 admin = User.find_or_create_by!(email: 'admin@example.com') do |user|
   user.password = 'password'
   user.password_confirmation = 'password'
+  user.username = 'admin' # 必要なフィールドに値を設定
   user.admin = true
 end
 
