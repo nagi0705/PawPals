992a5bc5c454f38bd4121f79043b419b86497d1e
EC2 Default User <ec2-user@ip-172-31-10-249.ap-northeast-1.compute.internal>
2024-07-15 07:46:30 +0000

    No

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -21,12 +21,12 @@ class Admin::UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     ActiveRecord::Base.transaction do
-      @user.group_memberships.destroy_all  # 関連するgroup_membershipsを削除
-      @user.destroy!
+      @user.group_memberships.destroy_all  # これで関連するgroup_membershipsを削除
+      @user.destroy
     end
     redirect_to admin_users_path, notice: 'User was successfully destroyed.'
-  rescue ActiveRecord::RecordNotDestroyed, ActiveRecord::RecordInvalid => e
-    redirect_to admin_users_path, alert: "Failed to delete user: #{e.message}"
+  rescue ActiveRecord::InvalidForeignKey
+    redirect_to admin_users_path, alert: 'Failed to destroy user due to foreign key constraint.'
   end
 
   private
