ca38b87620a5f97691580e4fad178348c3928da2
EC2 Default User <ec2-user@ip-172-31-10-249.ap-northeast-1.compute.internal>
2024-07-15 10:58:32 +0000

    Update puma configuration for production environment

--- config/puma.rb
+++ config/puma.rb
@@ -8,21 +8,16 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-#
-port ENV.fetch("PORT") { 3000 }
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
-
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
-#
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+# Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
 # Specifies the number of `workers` to boot in clustered mode.
@@ -31,21 +26,20 @@ plugin :tmp_restart
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 
-
 rails_root = Dir.pwd
-if Rails.env.production?
+if ENV.fetch("RAILS_ENV") { "development" } == "production"
   pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
   state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
   stdout_redirect(
@@ -53,7 +47,5 @@ if Rails.env.production?
     File.join(rails_root, 'log', 'puma-error.log'),
     true
   )
-  daemonize
+  daemonize true
 end
-
-
