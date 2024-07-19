78ae5ed446b669e9017b40c51f8378dff54e0ddc
EC2 Default User <ec2-user@ip-172-31-10-249.ap-northeast-1.compute.internal>
2024-07-19 03:49:27 +0000

    Add admin_id to Group model and update GroupsController

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,7 @@ class Group < ApplicationRecord
   has_many :users, through: :group_memberships
   has_many :posts
   has_many :messages
+  belongs_to :admin, class_name: "User", optional: true
 
   validates :name, presence: { message: "名前を入力してください。" }
   validates :description, presence: { message: "説明を入力してください。" }
